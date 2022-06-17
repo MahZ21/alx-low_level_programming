c133c19f55559470653c380741cd164c6f2c36d3
MahZ21 <Mahletz2711@gmail.com>
2022-06-17 21:39:13 +0300

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,24 +18,4 @@ char *_strncpy(char *dest, char *src, int n)
 	for ( ; i < n; ++i)
 		dest[i] = '\0';
 	return (dest);
-}#include "main.h"
-/**
- * _strncpy - making a copy.
- *@dest: high copy.
- *@src: main.
- *@n: print upto this.
- *Return: copy.
- */
-char *_strncpy(char *dest, char *src, int n)
-{
-	int i = 0;
-
-	while (src[i] && i < n)
-	{
-		dest[i] = src[i];
-		++i;
-	}
-	for ( ; i < n; ++i)
-		dest[i] = '\0';
-	return (dest);
 }
